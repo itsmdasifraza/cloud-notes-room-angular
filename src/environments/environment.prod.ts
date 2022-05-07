5a7bde720af76d5638738b005aabf4a86ac77eae
Md Asif Raza <itsmdasifraza@gmail.com>
2022-05-07 20:50:08 +0530

    login page ui upgraded

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,13 @@ export const environment = {
   backend: {
     connection : "https",
     ip : "cloud-notes-room.herokuapp.com"
+  },
+  app : {
+    name : "Cloud Notes Room",
+  },
+  developer : {
+    name : "Md Asif Raza",
+    position : "Software Developer",
+    email : "itsmdasifraza@gmail.com"
   }
 };
